f0e3c0998a9e9f78dd70b0013f83642a658d9ca6
nesosuke <neso@neso.tech>
2022-08-20 13:12:05 +0900

    get device list and status

--- ctl.py
+++ ctl.py
@@ -1,6 +1,10 @@
+from flask import render_template, Flask
+import sys
 import json
 import os
 import requests
+
+app = Flask(__name__)
 
 REFERENCE_URL = '''
 GitHub: https://github.com/OpenWonderLabs/SwitchBotAPI/blob/549798907aeff0504135a3f087affc63657d0207/README.md
@@ -58,23 +62,42 @@ def get_token():
     return token
 
 
-def get_device_list(token: str) -> json:
+def get_device_list() -> json:
     '''
     get device list from api
     '''
-    suffix = '/v1.0/devices'
-    url = API_URL + suffix
+    url = f'{API_URL}/devices'
 
-    headers = {
-        'Authorization': token,
-        'Content-Type': 'application/json; charset=utf-8'
-    }
-
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=HEADERS)
     return response.json()
 
 
+def get_device_status(device_id: str) -> json:
+    '''
+    get the status of a physical device from api.
+    '''
+    device_id = str(device_id)
+    url = f'{API_URL}/devices/{device_id}/status'
+
+    response = requests.get(url, headers=HEADERS)
+    return response.json()
+
+
+OPEN_TOKEN = get_token()
+HEADERS = {
+    'Authorization': OPEN_TOKEN,
+    'Content-Type': 'application/json; charset=utf-8'
+}
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
 # debug
+DEVICE_ID_DEBUG = 'C8AEB72F341A'
 if __name__ == "__main__":
-    token = get_token()
-    print(get_device_list(token))
+    result = get_device_status(DEVICE_ID_DEBUG)
+    print(result)
+    sys.exit(0)
